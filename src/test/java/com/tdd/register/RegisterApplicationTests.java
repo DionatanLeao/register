6e67b062da9d40fca1ddc595a85f1b9fa6c8468b
DionatanLeao <dionatan.ti@outlook.com>
2026-02-03 21:36:00 -0300

    feat: Creating the test

--- src/test/java/com/tdd/register/RegisterApplicationTests.java
+++ src/test/java/com/tdd/register/RegisterApplicationTests.java
@@ -1,13 +1,32 @@
 package com.tdd.register;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class RegisterApplicationTests {
+	@InjectMocks
+	private UserService userService;
+	@Mock
+	private UserRepository userRepository;
 
 	@Test
-	void contextLoads() {
+	void should_register_user_successfully() {
+		User userBeforeSave = new User("Danilo", "1234", LocalDate.of(1992, 10, 6));
+		User userAfterSave = userBeforeSave;
+		userAfterSave.setId("1");
+		Mockito.when(userRepository.save(user)).thenReturn(userAfterSave);
+
+		var user = userService.register(userBeforeSave);
+
+		Mockito.verify(userRepository).save(userBeforeSave);
+		Assertions.assertEquals(userAfterSave, user);
 	}
 
 }
