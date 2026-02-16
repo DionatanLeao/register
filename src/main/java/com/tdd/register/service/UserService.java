b7557d42bbdd966842c26eb9dbf54801f4cfe323
DionatanLeao <dionatan.ti@outlook.com>
2026-02-16 19:10:00 -0300

    feat: Creating the implementation - Part 1

--- src/main/java/com/tdd/register/service/UserService.java
+++ src/main/java/com/tdd/register/service/UserService.java
@@ -5,12 +5,21 @@ import com.tdd.register.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     public User register(User user) {
-        return userRepository.save(user);
+        var age = Period.between(user.getDateOfBirth(), LocalDate.now()).getYears();
+
+        if(age >= 18) {
+            return userRepository.save(user);
+        } else {
+            throw new RuntimeException("Idade não permitida");
+        }
     }
 }
