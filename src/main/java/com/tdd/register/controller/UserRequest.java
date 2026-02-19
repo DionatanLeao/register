1d3737d7f48c74d9910b82669cb93bd914abfac2
DionatanLeao <dionatan.ti@outlook.com>
2026-02-18 23:38:00 -0300

    feat: Creating the implementation

---
+++ src/main/java/com/tdd/register/controller/UserRequest.java
@@ -0,0 +1,24 @@
+package com.tdd.register.controller;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    @NotBlank
+    @Size(min = 3, max = 10, message = "The size should be between 3 and 10")
+    private String name;
+    @NotBlank
+    @Size(min = 4, max = 6, message = "The size should be between 4 and 6")
+    private String password;
+    @NotNull
+    private LocalDate dateOfBirth;
+}
